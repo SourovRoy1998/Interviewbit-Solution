384c9d0097a729153aa42b7263bc81b3b617a5cc
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-04-12 08:33:53 +0530

    Create binary-representation.cpp

---
+++ Bit Manipulation/binary-representation.cpp
@@ -0,0 +1,15 @@
+//https://www.interviewbit.com/problems/binary-representation/
+
+string Solution::findDigitsInBinary(int A) {
+    if(A==0)
+        return "0";
+    string s="";
+    while(A){
+        s = s+to_string(A%2);
+        A=A/2;
+    }
+    reverse(s.begin(),s.end());
+    return s;
+    
+}
+© 2019 GitHub, Inc.
