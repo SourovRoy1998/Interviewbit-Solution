a1fb7147781d434a90f308c1db2d93a3fe4ebcc4
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-04-12 19:25:48 +0530

    Create single-number.cpp

---
+++ Bit Manipulation/single-number.cpp
@@ -0,0 +1,8 @@
+//https://www.interviewbit.com/problems/single-number/
+
+int Solution::singleNumber(const vector<int> &A) {
+    int ans=0;
+    for(int i=0;i<A.size();i++)
+        ans^=A[i];
+    return ans;
+}
