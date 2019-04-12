4ab51fdc7de89f3e7286f73833d0dc8dc754842d
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-04-12 07:58:03 +0530

    Update number-of-1-bits.cpp

--- number-of-1-bits.cpp
+++ number-of-1-bits.cpp
@@ -1,5 +1,6 @@
 //https://www.interviewbit.com/problems/number-of-1-bits/
 
+//Iterative Solution
 int Solution::numSetBits(unsigned int A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
@@ -9,6 +10,21 @@ int Solution::numSetBits(unsigned int A) {
     while(A){
         count+=(A%2);
         A=A/2;
+    }
+    return count;
+}
+
+
+//Bit Manipulation
+int Solution::numSetBits(unsigned int A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int count=0;
+    while(A){
+        A=A&(A-1);
+        count++;
     }
     return count;
 }
