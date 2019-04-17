e303af50d095ede1018dc0c82987256ff3cd041a
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-04-17 09:29:48 +0530

    Create divide-integers.cpp

---
+++ Bit Manipulation/divide-integers.cpp
@@ -0,0 +1,26 @@
+//https://www.interviewbit.com/problems/divide-integers/
+
+
+int Solution::divide(int A, int B) {
+    long long dividend=A,divisor=B,quotient=0,temp=0;
+    bool sign=true;
+    if(dividend<0 ^ divisor<0)
+        sign=false;
+    
+    dividend=abs(dividend);
+    divisor=abs(divisor);
+    
+    for(int i=31;i>=0;i--){
+        if((temp+(divisor<<i))<=dividend){
+            temp+=(divisor<<i);
+            quotient|=(1LL<<i);
+        }
+    }
+    
+    if(!sign)
+        quotient=~quotient+1;
+    
+    if(quotient>=INT_MAX)
+        return INT_MAX;
+    return quotient;
+}
