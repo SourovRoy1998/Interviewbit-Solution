9842e663ff3d52f09ad1bb1ab2cc0549fe08c23b
Sourov Roy <37055760+SourovRoy1998@users.noreply.github.com>
2019-04-17 09:33:26 +0530

    Update divide-integers.cpp

--- Bit Manipulation/divide-integers.cpp
+++ Bit Manipulation/divide-integers.cpp
@@ -1,6 +1,7 @@
 //https://www.interviewbit.com/problems/divide-integers/
 
 
+//Bit Manipulation
 int Solution::divide(int A, int B) {
     long long dividend=A,divisor=B,quotient=0,temp=0;
     bool sign=true;
@@ -16,6 +17,28 @@ int Solution::divide(int A, int B) {
             quotient|=(1LL<<i);
         }
     }
+    
+    if(!sign)
+        quotient=~quotient+1;
+    
+    if(quotient>=INT_MAX)
+        return INT_MAX;
+    return quotient;
+}
+
+
+//Mathematics
+//https://www.geeksforgeeks.org/divide-two-integers-without-using-multiplication-division-and-mod-operator-set2/
+int Solution::divide(int A, int B) {
+    long long dividend=A,divisor=B,quotient=0,temp=0;
+    bool sign=true;
+    if(dividend<0 ^ divisor<0)
+        sign=false;
+    
+    dividend=abs(dividend);
+    divisor=abs(divisor);
+    
+    quotient = exp(log(dividend)-log(divisor));
     
     if(!sign)
         quotient=~quotient+1;
